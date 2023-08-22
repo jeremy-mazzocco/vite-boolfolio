a154c050f175ee540a8a75bec01ad21c0d4f28b0
Jeremy Mazzocco <68953007+jeremy-mazzocco@users.noreply.github.com>
2023-08-22 14:21:10 +0200

    add scafolding

--- vite-boolfolio/src/components/Projects.vue
+++ vite-boolfolio/src/components/Projects.vue
@@ -1,7 +1,49 @@
 <script>
-import { ref } from 'vue'
+import { ref } from 'vue';
+import axios from 'axios';
 
+export default {
+    name: 'Projects',
+    data: function () {
+        return {
+            projectes: [],
+            // pages: []
+        }
+    },
+    // methods: {
 
+    //     loadPage(target) {
+
+    //         if (target == null) return;
+
+    //         this.loadRabbits(target);
+    //     },
+    //     loadRabbits(target) {
+    //         axios.get(target)
+    //             .then(response => {
+
+    //                 const data = response.data;
+    //                 console.log(data);
+
+    //                 this.rabbits = data.rabbits.data;
+    //                 this.pages = data.rabbits.links;
+    //             })
+    //             .catch(error => {
+    //                 console.log(error);
+    //             });
+    //     }
+    // },
+    mounted() {
+
+        axios.get('http://localhost:8000/api/v1/projectsApi')
+            .then(response => {
+                console.log(reponse.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+}
 </script>
 
 <template>
