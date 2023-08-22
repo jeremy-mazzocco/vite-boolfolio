80371f9f03b27d97c7c2406c054bc5f70d042838
Jeremy Mazzocco <68953007+jeremy-mazzocco@users.noreply.github.com>
2023-08-22 17:17:51 +0200

    milestone 3

--- vite-boolfolio/src/components/Projects.vue
+++ vite-boolfolio/src/components/Projects.vue
@@ -7,37 +7,16 @@ export default {
     data: function () {
         return {
             projectes: [],
-            // pages: []
+
         }
     },
-    // methods: {
-
-    //     loadPage(target) {
-
-    //         if (target == null) return;
-
-    //         this.loadRabbits(target);
-    //     },
-    //     loadRabbits(target) {
-    //         axios.get(target)
-    //             .then(response => {
-
-    //                 const data = response.data;
-    //                 console.log(data);
-
-    //                 this.rabbits = data.rabbits.data;
-    //                 this.pages = data.rabbits.links;
-    //             })
-    //             .catch(error => {
-    //                 console.log(error);
-    //             });
-    //     }
-    // },
     mounted() {
 
-        axios.get('http://localhost/api/v1/projectsList')
+        axios.get('http://localhost:8000/api/v1/projects-list')
             .then(response => {
-                console.log(reponse.data);
+
+                this.projects = response.data.projects
+                console.log(response.data.projects);
             })
             .catch(error => {
                 console.log(error);
@@ -47,7 +26,12 @@ export default {
 </script>
 
 <template>
-    ciaooo
+    <h1>
+        Projects List
+    </h1>
+    <div>
+
+    </div>
 </template>
 
 <style scoped></style>
